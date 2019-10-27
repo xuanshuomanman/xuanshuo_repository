490dc7de8b9bcbd336318605ea188284de5cf269
萱说蛮蛮 <@DESKTOP-AFJ4MAG>
2019-10-27 11:43:16 +0800

    hello

---
+++ gitTest/src/main/java/com/hqyj/gitTest/test/testController/TestController.java
@@ -0,0 +1,13 @@
+package com.hqyj.gitTest.test.testController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping("/test/config")
+	public String test(){
+		return "hello";
+	}
+}
